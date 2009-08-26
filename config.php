9591241e3408f93b1eb880b8505891086b2393c6
Akshay Joshi <joshi.a@gmail.com>
2009-08-26 19:04:26 +0000

    Removes the ACL from config.php. Fixes #70.

--- config.php
+++ config.php
@@ -6,12 +6,7 @@ header("Cache-Control: no-cache, must-revalidate" );
 header("Pragma: no-cache" ); 
 
 $admins = array();
-$admins['AkshayJoshi'] = true;
-$admins['admin'] = true;
-
 $teachers = array();
-$teachers['teacher'] = TRUE;
-$teachers['CarolynClark'] = TRUE;$teachers['LaneGraciano'] = TRUE;$teachers['PaulSelf'] = TRUE;$teachers['MarkSommers'] = TRUE;$teachers['JocelynWiley'] = TRUE;$teachers['RobertAllan'] = TRUE;$teachers['LauraBerntson'] = TRUE;$teachers['OckiFernandes'] = TRUE;$teachers['DanielNicolson'] = TRUE;$teachers['JoannaRoberts'] = TRUE;$teachers['SamWerlinich'] = TRUE;$teachers['DrewDavis'] = TRUE;$teachers['SharonKerr'] = TRUE;$teachers['AmitKhanna'] = TRUE;$teachers['ShawnKrause'] = TRUE;$teachers['LindseyShoemaker'] = TRUE;$teachers['DavidBerntson'] = TRUE;$teachers['PaulBrocklehurst'] = TRUE;$teachers['GlendaFrank'] = TRUE;$teachers['DebJones'] = TRUE;$teachers['HanaBayyari'] = TRUE;$teachers['RajaBayyari'] = TRUE;$teachers['HanyaMikati'] = TRUE;$teachers['AliMirzo'] = TRUE;$teachers['RimaSarakbi'] = TRUE;$teachers['LaylaBlock'] = TRUE;$teachers['ValdirChagas'] = TRUE;$teachers['ManalYoussef'] = TRUE;$teachers['ClaudiaGonzalez'] = TRUE;$teachers['ValiaJimenez'] = TRUE;$teachers['MercedesdeRamos'] = TRUE;$teachers['LorettaMazzuchin'] = TRUE;$teachers['LyndaHalabi'] = TRUE;$teachers['MarthaJensen'] = TRUE;$teachers['JohnSalminen'] = TRUE;$teachers['NicolasPavlos'] = TRUE;$teachers['DonnaAllen'] = TRUE;$teachers['JasmineBrawn'] = TRUE;$teachers['HajeHalabi'] = TRUE;$teachers['BradleyNewell'] = TRUE;$teachers['AnneRussell'] = TRUE;$teachers['MarkHopkin'] = TRUE;$teachers['AbhayaNivarthi'] = TRUE;$teachers['JesseRemington'] = TRUE;
 
 $base_path = dirname($_SERVER['SCRIPT_FILENAME']);
 
@@ -27,11 +22,12 @@ $time_increments = (10*60);
 
 // Only use one authentication method. Comment out the unused ones.
 $auth = array();
-/*$auth['ldap'] = array('host' => 'home.acs.sch.ae',
-					  'port' => NULL,
-					  'basedn' => 'dc=home,dc=acs,dc=sch,dc=ae',
-					 );*/
-$auth['test'] = array();
+$auth['ldap'] = array(
+  'host' => 'home.acs.sch.ae',
+  'port' => NULL,
+  'basedn' => 'dc=home,dc=acs,dc=sch,dc=ae',
+);
+//$auth['test'] = array();
 
 $db_url = 'sqlite:db.sqlite';
 
