b5f842543952e67ef4a97a7c434ef259e2ee1905
Akshay Joshi <joshi.a@gmail.com>
2010-03-12 17:31:58 +0400

    Switched to using heredocs so that the HTML output at the end is also readable.

--- plugins/template.php
+++ plugins/template.php
@@ -41,29 +41,31 @@ class Template {
   }
 
   private function renderHeader() {
-    $return = '<?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
-      <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-      <head>
-      <title>'.$this->title.' | '.$this->site_name.'</title>
-      <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-      <link rel="stylesheet" type="text/css" media="screen" href="stylesheets/grid.css"  />
-      <link rel="stylesheet" type="text/css" media="screen" href="stylesheets/application.css" />
-      <link rel="stylesheet" type="text/css" media="print"  href="stylesheets/print.css" />
-      <script type="text/javascript" src="scripts/jquery.js"></script>
-      <script type="text/javascript" src="scripts/jquery-ui.js"></script>
-      <script type="text/javascript" src="scripts/jquery-form.js"></script>
-      <script type="text/javascript" src="scripts/application.js"></script>
-      '.$this->scripts.'
-      </head>
-      <body>
-      <div class="container_12">
+    $return = <<<EOT
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <title>$this->title | $this->site_name</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <link rel="stylesheet" type="text/css" media="screen" href="stylesheets/grid.css"  />
+    <link rel="stylesheet" type="text/css" media="screen" href="stylesheets/application.css" />
+    <link rel="stylesheet" type="text/css" media="print"  href="stylesheets/print.css" />
+    <script type="text/javascript" src="scripts/jquery.js"></script>
+    <script type="text/javascript" src="scripts/jquery-ui.js"></script>
+    <script type="text/javascript" src="scripts/jquery-form.js"></script>
+    <script type="text/javascript" src="scripts/application.js"></script>
+    $this->scripts
+  </head>
+  <body>
+    <div class="container_12">
       <div class="alpha omega" id="header">
-	      <h2><a href="index.php" title="Home">'.$this->site_name.'</a></h2>
+        <h2><a href="index.php" title="Home">$this->site_name</a></h2>
       </div>
       <div id="textos">
-    ';
-    $return  = $return.'<h1>'.$this->title.'</h1>';
+        <h1>$this->title</h1>
+
+EOT;
     return $return;
   }
 
@@ -74,13 +76,17 @@ class Template {
       $time = $this->debug['time']['end'] - $this->debug['time']['start'];
       $debug_msg = 'Peak PHP memory usage: '.$mem_peak.' MB. Total request time: '.$time.' s.';
     }
-    $return = '
-      <div id="dialog"></div>
+    $return = <<<EOT
+
+        <div id="dialog"></div>
       </div>
-      <div id="copy">&copy; 2009-2010 <a href="http://akshayjoshi.com">Akshay Joshi</a>. Licensed under the <a href="http://creativecommons.org/licenses/MIT/">MIT License</a>. '.$debug_msg.'</div>
+      <div id="copy">
+        &copy; 2009-2010 <a href="http://akshayjoshi.com">Akshay Joshi</a>. Licensed under the <a href="http://creativecommons.org/licenses/MIT/">MIT License</a>. $debug_msg
       </div>
-      </body>
-      </html>';
+    </div>
+  </body>
+</html>
+EOT;
     return $return;
   }
 
