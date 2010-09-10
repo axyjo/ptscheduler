cf658ab0caf86daf4ed38387c495fe2400497e30
Akshay Joshi <joshi.a@gmail.com>
2010-09-10 19:19:06 +0400

    Added new ROOT variable and the bootstrap file.

--- index.php
+++ index.php
@@ -1,37 +1,33 @@
 <?php
 
-$admins = array();
-$teachers = array();
-$date_boundaries = array();
-$time_boundaries = array();
-$auth = array();
-// This isn't __DIR__ because it isn't supported in PHP < 5.3.0
-$base_path = dirname(__FILE__);
+define('ROOT', getcwd());
+require_once ROOT.'plugins/bootstrap.php';
+bootstrap();
 
 // Check for the template engine before attemptig to load it.
-if(!file_exists($base_path.'/plugins/template.php')) {
+if(!file_exists(ROOT.'/plugins/template.php')) {
   echo 'The template engine does not exist. Please redownload this application.';
   exit();
 } else {
-  require($base_path.'/plugins/template.php');
+  require(ROOT.'/plugins/template.php');
   $template = Template::getInstance();
 }
 
 // Check for configuration file before attempting to load it
-if(!file_exists($base_path.'/config.php')) {
+if(!file_exists(ROOT.'/config.php')) {
   $template->setTitle('Configuration not found');
   $template->setContent('A configuration file was not found. Please copy <code>default.config.php</code> to <code>config.php</code>. After that, please modify <code>config.php</code> so that it has the right settings for your purposes.');
   $template->render();
   exit();
 } else {
-  require($base_path.'/config.php');
+  require(ROOT.'/config.php');
   // Set site title now since it wasn't available before.
   $template->setSiteName($site_name);
 }
 
 // Check for all of the other required files without reverting to PHP's default
 // white page of errors.
-$required_files = array($base_path.'/plugins/session.php', $base_path.'/plugins/db.php', $base_path.'/plugins/time.php');
+$required_files = array(ROOT.'/plugins/session.php', ROOT.'/plugins/db.php', ROOT.'/plugins/time.php');
 $return = '<div class="error"><ul>';
 $stop = FALSE;
 foreach($required_files as $file) {
@@ -71,7 +67,7 @@ try {
   $check = 'SELECT 1 FROM users';
   $check_res = $dbHandle->query($check);
 } catch (PDOException $e) {
-  include($base_path.'/install.php');
+  include(ROOT.'/install.php');
   exit;
 }
 unset($admins, $teachers);
@@ -79,28 +75,28 @@ unset($admins, $teachers);
 if($user_access == USER_FORBIDDEN) {
   // Send a login screen to unauthenticated users.
   if(isset($_GET['login'])) {
-    include($base_path.'/plugins/auth.php');
+    include(ROOT.'/plugins/auth.php');
   } else {
-    include($base_path.'/views/login.php');
+    include(ROOT.'/views/login.php');
   }
 } else {
   if(isset($_GET['logout'])) {
-    include($base_path.'/plugins/auth.php');
+    include(ROOT.'/plugins/auth.php');
   } elseif(isset($_GET['add']) || isset($_GET['delete']) || isset($_GET['form']) ) {
-    include($base_path.'/form.php');
+    include(ROOT.'/form.php');
     exit;
   } else {
     //this is the home page
     if ($user_access == USER_ADMIN) {
       if(isset($_GET['list'])) {
-        include($base_path.'/views/list.php');
+        include(ROOT.'/views/list.php');
       } else {
-        include($base_path.'/views/admin.php');
+        include(ROOT.'/views/admin.php');
       }
     } elseif ($user_access == USER_TEACHER) {
-      include($base_path.'/views/teacher.php');
+      include(ROOT.'/views/teacher.php');
     } elseif ($user_access == USER_PARENT) {
-      include($base_path.'/views/parent.php');
+      include(ROOT.'/views/parent.php');
     }
   }
 }
