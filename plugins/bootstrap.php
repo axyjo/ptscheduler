366e0ae42de078ad0c496975b9030a6be6778360
Akshay Joshi <joshi.a@gmail.com>
2010-09-11 16:40:57 +0400

    Moved views code to bootstrap.

--- plugins/bootstrap.php
+++ plugins/bootstrap.php
@@ -85,3 +85,32 @@ try {
   include(ROOT.'/install.php');
   exit;
 }
+
+if($user_access == USER_FORBIDDEN) {
+  // Send a login screen to unauthenticated users.
+  if(isset($_GET['login'])) {
+    include(ROOT.'/plugins/auth.php');
+  } else {
+    include(ROOT.'/views/login.php');
+  }
+} else {
+  if(isset($_GET['logout'])) {
+    include(ROOT.'/plugins/auth.php');
+  } elseif(isset($_GET['add']) || isset($_GET['delete']) || isset($_GET['form']) ) {
+    include(ROOT.'/form.php');
+    exit;
+  } else {
+    //this is the home page
+    if ($user_access == USER_ADMIN) {
+      if(isset($_GET['list'])) {
+        include(ROOT.'/views/list.php');
+      } else {
+        include(ROOT.'/views/admin.php');
+      }
+    } elseif ($user_access == USER_TEACHER) {
+      include(ROOT.'/views/teacher.php');
+    } elseif ($user_access == USER_PARENT) {
+      include(ROOT.'/views/parent.php');
+    }
+  }
+}
