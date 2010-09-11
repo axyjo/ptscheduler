978b5c2d512bdf3913d7da7d20397c47a4d28479
Akshay Joshi <joshi.a@gmail.com>
2010-09-11 18:43:01 +0400

    Use the _grid partial in parent.

--- views/parent.php
+++ views/parent.php
@@ -31,59 +31,8 @@ if (count($appointments) == 0) $return .= 'Sorry, you do not have any appointmen
 
 $tabular_times = tabularTimes();
 
-$return .= '<div id="time_grid">';
-// TODO: Render the _grid.php partial here.
-foreach($teachers as $teacher) {
-  $sql = 'SELECT * FROM appointments WHERE teacher='.$teacher['id'];
-  $app_res = $dbHandle->query($sql);
-  $appointments = array();
-  while($result = $app_res->fetch()) {
-    $appointments[$result['time']][] = $result;
-  }
-  $return .= '<div id="'.$teacher['id'].'">';
-  $return .= '<span class="teacher grid_6"><strong>';
-  $return .= $teacher['fname'].' '.$teacher['lname'];
-  $return .= '</strong > - <a id="link_'.$teacher['id'].'" onclick=\'$("#times_'.$teacher['id'].'").slideToggle();\'>Click here to view available appointments</a></span><br />
-    <div class="grid_2 throbber" id="throbber_'.$teacher['id'].'"></div>
-    <div style="display:none;" id="times_'.$teacher['id'].'">';
-  foreach($tabular_times as $minute => $hours_array) {
-    foreach($hours_array as $hour => $epoch) {
-      if(!isset($appointments[$epoch]) || !is_array($appointments[$epoch])) {
-        $appointments[$epoch] = array();
-        $class = 'green';
-        $title = 'Available';
-        if($simultaneous_appointments > 1) {
-          $title .= ' ('.$simultaneous_appointments.'/'.$simultaneous_appointments.')';
-        }
-      }
-      $count = count($appointments[$epoch]);
-      if($count < $simultaneous_appointments) {
-        foreach($appointments[$epoch] as $appointment) {
-          if($appointment['parent'] == -1) {
-            $class = 'red';
-            $title = 'Unavailable';
-            break;
-          }
-          $class = 'green';
-          $title = 'Available';
-          if($simultaneous_appointments > 1) {
-            $title .= ' ('.($simultaneous_appointments-$count).'/'.$simultaneous_appointments.')';
-          }
-        }
-      } else {
-        $class = 'red';
-        $title = 'Unavailable';
-      }
-
-      $time = $hour.$minute;
-
-      $return .= '<span title="'.$title.'" class="'.$class.' times grid_1 push_2" id="'.$teacher['id'].'-'.$epoch.'">'.$time.'</span>';
-    }
-    $return .= '<br />';
-  }
-  $return .= '</div></div>';
+foreach($teacher_list as $teacher) {
+  include(ROOT.'/views/_grid.php');
 }
-
-$return .= '</div>';
 
 $template->setContent($return);
