49e84661e701341e9281b901021039eeb271a979
Akshay Joshi <joshi.a@gmail.com>
2010-09-11 18:33:43 +0400

    Changed teacher view to use the _grid partial.

--- views/teacher.php
+++ views/teacher.php
@@ -34,69 +34,7 @@ if (count($appointments) == 0) $return .= 'Sorry, you do not have any appointmen
 
 $tabular_times = tabularTimes();
 
-// TODO: Render the _grid.php partial here.
-
-$sql = 'SELECT * FROM appointments WHERE teacher='.$user_id;
-$app_res = $dbHandle->query($sql);
-$appointments = array();
-while($result = $app_res->fetch()) {
-  if(!isset($appointments[$result['time']]) || !is_array($appointments[$result['time']])) {
-    $appointments[$result['time']] = array();
-  }
-  $appointments[$result['time']][] = $result;
-}
-
-$teacher = getUser($user_id);
-$return .= '<div id="'.$user_id.'">';
-$return .= '<span class="teacher grid_6"><strong>';
-$return .= $teacher['fname'].' '.$teacher['lname'];
-$return .= '</strong ></span><br />
-  <div class="grid_2 throbber" id="throbber_'.$user_id.'"></div>
-  <div id="times_'.$user_id.'">';
-foreach($tabular_times as $minute => $hours_array) {
-  foreach($hours_array as $hour => $epoch) {
-    if(!isset($appointments[$epoch]) || !is_array($appointments[$epoch])) {
-      $appointments[$epoch] = array();
-      $class = 'green';
-      $title = 'Available';
-      if($simultaneous_appointments > 1) {
-        $title .= ' ('.$simultaneous_appointments.'/'.$simultaneous_appointments.')';
-      }
-    }
-    $count = count($appointments[$epoch]);
-    if($count < $simultaneous_appointments) {
-      foreach($appointments[$epoch] as $appointment) {
-        if($appointment['parent'] == -1) {
-          $class = 'yellow';
-          $title = 'Break';
-          break;
-        }
-        $class = 'green';
-        $title = 'Available';
-        if($simultaneous_appointments > 1) {
-          $title .= ' ('.($simultaneous_appointments-$count).'/'.$simultaneous_appointments.')';
-        }
-      }
-    } else {
-      $class = 'red';
-      $title = 'Appointment with: ';
-      foreach($appointments[$epoch] as $appointment) {
-        $parent = getUser($appointment['parent']);
-        $title .= $parent['lname'].' ('.$parent['desc'].')';
-        if($appointment['parent'] == -1) {
-          $class = 'yellow';
-          $title = 'Break';
-          break;
-        }
-      }
-    }
-
-    $time = $hour.$minute;
-
-    $return .= '<span title="'.$title.'" class="'.$class.' times grid_1 push_2" id="'.$teacher['id'].'-'.$epoch.'">'.$time.'</span>';
-  }
-  $return .= '<br />';
-}
-$return .= '</div></div>';
+$teacher = getUser($_SESSION['user_id']);
+include(ROOT.'/views/_grid.php');
 
 $template->setContent($return);
