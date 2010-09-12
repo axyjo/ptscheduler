6af1f0ce3b2ea7a868b2140217d2fc5322a68c45
Akshay Joshi <joshi.a@gmail.com>
2010-09-12 09:16:02 +0400

    Added the description field on mouseover to the teacher and admin views.

--- views/_appointments.php
+++ views/_appointments.php
@@ -17,11 +17,15 @@ while ($result = $result_res->fetch()) {
   $appointments[] = $result;
   if ($user['status'] == USER_PARENT) {
     $other_party = getUser($result['teacher']);
+    // Clear the description for a teacher.
+    $other_party['description'] = '';
   } else {
-    // This user is a teacher.
+    // This user is a teacher or administrator.
     $other_party = getUser($result['parent']);
   }
-  $return .= date($date_format, $result['time']) . ' - ' . $other_party['fname'] . ' ' . $other_party['lname'];
+  $return .= date($date_format, $result['time']) . ' - <a title="' . $other_party['description'] .'">';
+  $return .= $other_party['fname'] . ' ' . $other_party['lname'];
+  $return .= '</a> (<a href="mailto:' . $other_party['email'] . '">Email</a>)';
   $return .= '<br />';
 }
 
