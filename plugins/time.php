2267c225ca0a8710e79e893833f42cddcc4a20ab
Majed Itani <mitani@gmail.com>
2011-10-11 12:44:24 -0700

    Fixes an index undefined issue

--- plugins/time.php
+++ plugins/time.php
@@ -9,14 +9,16 @@ function returnTimes() {
 
   foreach($date_boundaries as $date => $test) {
   	$date_parts = explode('-', $date);
-  	$day = mktime(0,0,0,$date_parts[1], $date_parts[2], $date_parts[0]);
-  	$start = $day + $time_boundaries['start'];
-  	$current = $start;
-  	$end = $day+$time_boundaries['end'];
-  	while($current < $end) {
-  	  $times[] = $current;
-  	  $current += $time_increments;
-  	}
+  	if(count($date_parts) > 2){
+    	$day = mktime(0,0,0,$date_parts[1], $date_parts[2], $date_parts[0]);
+  		$start = $day + $time_boundaries['start'];
+  		$current = $start;
+  		$end = $day+$time_boundaries['end'];
+  		while($current < $end) {
+  			  $times[] = $current;
+  	 		 $current += $time_increments;
+  		}
+	  }
   }
   return $times;
 }
